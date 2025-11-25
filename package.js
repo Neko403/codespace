d15827c61dc90f23c572f3e1b08fe89fb7f78203
Neko403 <nenekori@protonmail.com>
2025-11-25 16:47:52 +0700

    Update package.js

--- package.js
+++ package.js
@@ -2,18 +2,16 @@
   "name": "youtube-music-api",
   "version": "1.0.0",
   "description": "API untuk download audio YouTube",
-  "main": "api.js",
+  "main": "api/play.js",
   "scripts": {
-    "start": "node api.js",
-    "dev": "nodemon api.js"
+    "dev": "vercel dev",
+    "start": "node server.js"
   },
   "dependencies": {
-    "express": "^4.18.2",
-    "cors": "^2.8.5",
     "yt-search": "^2.10.4",
     "node-fetch": "^2.6.7"
   },
-  "devDependencies": {
-    "nodemon": "^3.0.1"
+  "engines": {
+    "node": "18.x"
   }
 }
